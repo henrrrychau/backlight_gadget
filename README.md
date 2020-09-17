95cb341927889ec2e28c9d4ab990f4ddbc7e8d86
HinWaei <1624339284@qq.com>
2020-09-17 16:37:11 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # backlight_gadget
 A gadget for backlight adjustment when encountered an ACPI BIOS ERROR(bug) on Ubuntu 19.0
+
+## Prerequisites
+**You need to have the right of accessing as root!**
+This script adjusts backlight by modifying /sys/class/back_
+
+```bash
+$ git clone https://github.com/HinWaei/backlight_gadget
+$ cd backlight_gadget && chmod +x ./backlight_gadget.sh
+$ sudo su
+...(Enter your password)
+\# ./backlight_gadget.sh
+```
---
+++ backlight_gadget.sh
@@ -0,0 +1,26 @@
+#-- Ubuntu Backlight Gadget BY:Hinux Chau --#
+#!/bin/bash
+
+if [[ $(whoami) != 'root' ]]; then
+	echo -e "\nPlease enter sudo su to run me as root";
+	exit;
+fi
+
+bright=1;
+max_bright=$( expr $(cat /sys/class/backlight/intel_backlight/max_brightness) / 10000 );
+while [[ 1 ]]; do
+	echo -e "\nPlease input brightness(integer 1-$max_bright) or send q/Q to quit:";
+	read bright;
+	if [[ $bright == 'q' ]] || [[ $bright == 'Q' ]]; then
+		echo -e '\nBye!~';
+		exit;
+	fi
+	if [[ $bright -lt 1 ]] || [[ $bright -gt $max_bright ]]; then
+		echo -e '\n**ERROR! Please Enter Correct Integer(2-$max_bright)**'
+		continue;
+	fi
+	echo $(( $bright * 10000 )) > /sys/class/backlight/intel_backlight/brightness
+	echo -e '\nSuccess!';
+done
+
+echo -e '\nBye~~'
